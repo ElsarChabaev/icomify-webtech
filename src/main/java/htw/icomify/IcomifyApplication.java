a6649efee8f1b8f4aeda4ada48db726268f0b2cc
Elsar Chabaev <s0561238@htw-berlin.de>
2022-11-17 00:01:23 +0100

    first commit - init

---
+++ src/main/java/htw/icomify/IcomifyApplication.java
@@ -0,0 +1,13 @@
+package htw.icomify;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IcomifyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IcomifyApplication.class, args);
+	}
+
+}
