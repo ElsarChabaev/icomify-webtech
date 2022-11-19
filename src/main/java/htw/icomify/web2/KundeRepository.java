40980ca17701adb6c8d4be43d0d1724ca11d9863
Elsar Chabaev <s0561238@htw-berlin.de>
2022-11-19 23:31:53 +0100

    @Repository

--- src/main/java/htw/icomify/web2/KundeRepository.java
+++ src/main/java/htw/icomify/web2/KundeRepository.java
@@ -1,5 +1,7 @@
 package htw.icomify.web2;
 
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface KundeRepository extends CrudRepository<Kunde, Long> {}
